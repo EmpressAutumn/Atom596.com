9f64aa9a5b4e65e1e94c41d5e2f8e90bf5af3b18
Autumn <adamhoffensetz@gmail.com>
2025-05-02 13:20:19 -0400

    Made the content section scale for readability

--- newlook/script/common.js
+++ newlook/script/common.js
@@ -1,5 +1,25 @@
-fetch("topbar.html")
-    .then(response => response.text())
-    .then((data) => {
-        document.getElementById("topbar").innerHTML = data;
-    });
+function insertTopbar() {
+    fetch("topbar.html")
+        .then(response => response.text())
+        .then((data) => {
+            document.getElementById("topbar").innerHTML = data;
+        });
+}
+
+function checkMobile() {
+    if(window.innerWidth/window.innerHeight < 1) {
+        document.getElementById("content").style.width = "calc(100% - 18px)";
+        document.getElementById("content").style.left = "calc(0% + 9px)";
+    } else {
+        document.getElementById("content").style.width = "calc(70% - 18px)";
+        document.getElementById("content").style.left = "calc(15% + 9px)";
+    }
+    console.log(window.innerWidth/window.innerHeight);
+}
+
+insertTopbar();
+checkMobile();
+
+window.addEventListener("resize", function() {
+    checkMobile();
+})
