141cf25766a6cea7a610d86d3a58b920225f7a99
Autumn <adamhoffensetz@gmail.com>
2025-05-02 12:35:30 -0400

    Realign content in the new look page

--- newlook/script/common.js
+++ newlook/script/common.js
@@ -2,8 +2,4 @@ fetch("topbar.html")
     .then(response => response.text())
     .then((data) => {
         document.getElementById("topbar").innerHTML = data;
-        const toggleButton = document.getElementById("mode-toggle");
-        if (toggleButton) {
-            toggleButton.addEventListener("click", toggleMode);
-        }
     });
