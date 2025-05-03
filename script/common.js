43887965baad481ae098b369ddc2e410ad069d14
Autumn <adamhoffensetz@gmail.com>
2025-05-02 20:28:05 -0400

    Change main page to new look format

--- script/common.js
+++ script/common.js
@@ -1,39 +1,24 @@
-window.addEventListener("load", function() {
+function insertTopbar() {
     fetch("/topbar.html")
         .then(response => response.text())
         .then((data) => {
             document.getElementById("topbar").innerHTML = data;
-            const toggleButton = document.getElementById("mode-toggle");
-            if (toggleButton) {
-                toggleButton.addEventListener("click", toggleMode);
-            }
-        })
+        });
+}
+
+function checkMobile() {
+    if(window.innerWidth/window.innerHeight < 1) {
+        document.getElementById("content").style.width = "100%";
+        document.getElementById("content").style.left = "0%";
+    } else {
+        document.getElementById("content").style.width = "70%";
+        document.getElementById("content").style.left = "15%";
+    }
+}
+
+insertTopbar();
+checkMobile();
+
+window.addEventListener("resize", function() {
+    checkMobile();
 })
-
-function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(";").shift();
-}
-
-function setCookie(name, value, days) {
-    const date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    const expires = `expires=${date.toUTCString()}`;
-    document.cookie = `${name}=${value}; ${expires}; path=/`;
-}
-
-function toggleMode() {
-    const currentMode = getCookie("theme") || "light";
-    const newMode = currentMode === "light" ? "dark" : "light";
-    setCookie("theme", newMode, 365);
-    location.reload();
-}
-
-function applyTheme() {
-    const theme = getCookie("theme") || "light";
-    const themeCss = document.getElementById("theme-css");
-    themeCss.href = "/style/" + theme + ".css";
-}
-
-applyTheme();
