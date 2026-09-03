767ce014f605a60fa457c23a97ddc32fcd6dd7ba
Autumn <adamhoffensetz@gmail.com>
2026-09-03 01:20:45 -0400

    Remove old homepage and create new navbar
    - Created a new navbar in Svelte
    - Removed the old website's homepage
    - Redirected the old navbar's home button to the new homepage
    - Began work on new webpage's design

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,49 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
     import "$lib/styles/global.css"
+    import NavButton from "./elements/NavButton.svelte";
 
 	let { children } = $props();
+
+    import { onMount } from "svelte";
+
+    let topbar: HTMLDivElement;
+
+    onMount(() => {
+        const updateHeight = () => {
+            document.documentElement.style.setProperty(
+                "--topbar-height",
+                `${topbar.getBoundingClientRect().height}px`
+            );
+        };
+
+        const observer = new ResizeObserver(updateHeight);
+        observer.observe(topbar);
+
+        updateHeight();
+
+        return () => observer.disconnect();
+    });
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<h1>Atom596.com</h1>
+<div id="top-section" bind:this={topbar}>
+    <p id="top-title">Atom596.com</p>
+    <div id="navbar">
+        <!-- <p id="navbar-title">Atom596.com</p> -->
+        <NavButton text="Home" onClick={() => { window.location.href = "/"; }} />
+        <NavButton text="OLD: Dev Notes" onClick={() => { window.location.href = "/devnotes/index.html"; }} />
+        <!-- <div style="background:red;width:240px;display:inline-block;">
+            <NavButton text="Settings" onClick={() => {}} />
+        </div> -->
+    </div>
+</div>
 
-{@render children()}
+<div id="content-section">
+    <div id="contents">
+        {@render children()}
+    </div>
+</div>
